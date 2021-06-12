dc6d6f38858af5113c7119294237515bb7be19a9
Breno Gazzola <breno.gazzola@gmail.com>
2021-06-12 15:56:37 -0300

    Bump to 0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## [Unreleased]
 
-## [0.1.0] - 2021-05-01
+## [0.2] - 2021-05-01
+
+- Added `opaque` metadata value to images
+
+## [0.1.3] - 2021-05-01
 
 - Initial release
--- lib/active_analysis/gem_version.rb
+++ lib/active_analysis/gem_version.rb
@@ -8,8 +8,8 @@ module ActiveAnalysis
 
   module VERSION
     MAJOR = 0
-    MINOR = 1
-    TINY  = 3
+    MINOR = 2
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
