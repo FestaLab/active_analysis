026a022fc224031988bf88847079e521fbcf9861
Breno Gazzola <breno.gazzola@gmail.com>
2021-06-17 16:05:53 -0300

    ASD

--- test/addon/image_addon/optimal_quality_test.rb
+++ test/addon/image_addon/optimal_quality_test.rb
@@ -2,15 +2,21 @@ require "test_helper"
 
 module ActiveAnalysis
   class Addon::ImageAddon::OptimalQualityTest < ActiveSupport::TestCase
-    setup do
-      ActiveAnalysis.addons = [Addon::ImageAddon::OptimalQuality]
-    end
-
     test "that it detects the optimal quality for the image" do
-      blob = create_file_blob(filename: "racecar.jpg", content_type: "image/jpeg")
-      metadata = extract_metadata_from(blob)
+      with_optimal_image_addon do
+        blob = create_file_blob(filename: "racecar.jpg", content_type: "image/jpeg")
+        metadata = extract_metadata_from(blob)
 
-      assert_equal 70, metadata[:optimal_quality]
+        assert_equal 70, metadata[:optimal_quality]
+      end
     end
+
+    private
+      def with_optimal_image_addon
+        previous_addons, ActiveAnalysis.addons = ActiveAnalysis.addons, [Addon::ImageAddon::OptimalQuality]
+        yield
+      ensure
+        ActiveAnalysis.addons = previous_addons
+      end
   end
 end
