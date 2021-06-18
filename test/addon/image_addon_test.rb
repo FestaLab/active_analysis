028b30dbbf4f400830222a39a99824fc5e876d4b
Breno Gazzola <breno.gazzola@gmail.com>
2021-06-18 09:34:48 -0300

    Do not execute addons in blobs of variant records

--- test/addon/image_addon_test.rb
+++ test/addon/image_addon_test.rb
@@ -9,5 +9,12 @@ module ActiveAnalysis
       blob = create_file_blob(filename: "audio.mp3", content_type: "audio/mp3")
       assert_not Addon::ImageAddon.accept?(blob)
     end
+
+    test "that it does not accept variant records" do
+      blob = create_file_blob(filename: "racecar.jpg", content_type: "image/jpeg")
+      blob = blob.variant(resize_to_limit: [100, 100]).processed.blob
+
+      assert Addon::ImageAddon.accept?(blob)
+    end
   end
 end
